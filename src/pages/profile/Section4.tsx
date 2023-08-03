e5047b97a048ffe08a38fd28c0e31163d3671bb8
Mark <muchirimark2@gmail.com>
2023-08-03 15:27:14 +0300

    done

--- src/pages/profile/Section4.tsx
+++ src/pages/profile/Section4.tsx
@@ -5,17 +5,17 @@ export default function Section4() {
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-16 md:mx-36 lg:gap-10 gap-6">
                     {/* First Image */}
                     <div className="bg-gray-300 p-0">
-                        <img src="https://lh3.googleusercontent.com/_Eb6cbVheT7vRVnuKfRA_8XQKqIf5-qWChuZiq5cD97EjUpNbhN8fpwtGJY-6N8G-cixnPLGso5TzhQIwgA2MI5il8EfP54Wt9nDCQnIDdwQuz_omg028ZD7Kc2bJxxFmha2fmrH-2eCA-pa26y2yi77LyrU85ux-wRGDNeaKn7NgJ6DGRHfDD7JcwUS4ADI=s2048" alt="Image 1" className="w-72 h-auto" />
+                        <img src="https://lh3.googleusercontent.com/_Eb6cbVheT7vRVnuKfRA_8XQKqIf5-qWChuZiq5cD97EjUpNbhN8fpwtGJY-6N8G-cixnPLGso5TzhQIwgA2MI5il8EfP54Wt9nDCQnIDdwQuz_omg028ZD7Kc2bJxxFmha2fmrH-2eCA-pa26y2yi77LyrU85ux-wRGDNeaKn7NgJ6DGRHfDD7JcwUS4ADI=s2048" alt="Image 1" className="w-full h-auto" />
                     </div>
 
                     {/* Second Image */}
                     <div className="bg-gray-300 p-0">
-                        <img src="https://lh3.googleusercontent.com/_Eb6cbVheT7vRVnuKfRA_8XQKqIf5-qWChuZiq5cD97EjUpNbhN8fpwtGJY-6N8G-cixnPLGso5TzhQIwgA2MI5il8EfP54Wt9nDCQnIDdwQuz_omg028ZD7Kc2bJxxFmha2fmrH-2eCA-pa26y2yi77LyrU85ux-wRGDNeaKn7NgJ6DGRHfDD7JcwUS4ADI=s2048" alt="Image 2" className="w-72 h-auto" />
+                        <img src="https://lh3.googleusercontent.com/_Eb6cbVheT7vRVnuKfRA_8XQKqIf5-qWChuZiq5cD97EjUpNbhN8fpwtGJY-6N8G-cixnPLGso5TzhQIwgA2MI5il8EfP54Wt9nDCQnIDdwQuz_omg028ZD7Kc2bJxxFmha2fmrH-2eCA-pa26y2yi77LyrU85ux-wRGDNeaKn7NgJ6DGRHfDD7JcwUS4ADI=s2048" alt="Image 2" className="w-full h-auto" />
                     </div>
 
                     {/* Third Image */}
                     <div className="bg-gray-300 p-0">
-                        <img src="https://lh3.googleusercontent.com/_Eb6cbVheT7vRVnuKfRA_8XQKqIf5-qWChuZiq5cD97EjUpNbhN8fpwtGJY-6N8G-cixnPLGso5TzhQIwgA2MI5il8EfP54Wt9nDCQnIDdwQuz_omg028ZD7Kc2bJxxFmha2fmrH-2eCA-pa26y2yi77LyrU85ux-wRGDNeaKn7NgJ6DGRHfDD7JcwUS4ADI=s2048" alt="Image 3" className="w-72 h-auto" />
+                        <img src="https://lh3.googleusercontent.com/_Eb6cbVheT7vRVnuKfRA_8XQKqIf5-qWChuZiq5cD97EjUpNbhN8fpwtGJY-6N8G-cixnPLGso5TzhQIwgA2MI5il8EfP54Wt9nDCQnIDdwQuz_omg028ZD7Kc2bJxxFmha2fmrH-2eCA-pa26y2yi77LyrU85ux-wRGDNeaKn7NgJ6DGRHfDD7JcwUS4ADI=s2048" alt="Image 3" className="w-full h-auto" />
                     </div>
                 </div>
             </div>
